f5b73fa71dbb38d29a2de45d8fe0dab8be3b35c4
Aleksi Sapon <aleksi@sapon.ca>
2017-02-19 14:19:27 -0500

    Add the Weeder adaptor (empty for now)

---
+++ src/main/java/ca/sapon/golite/Weeder.java
@@ -0,0 +1,14 @@
+package ca.sapon.golite;
+
+import golite.analysis.DepthFirstAdapter;
+
+/**
+ * TODO: weeding of
+ * 1. Assignment LHS
+ * 2. Expression statements
+ * 3. Default case
+ * 4. Break and continue
+ * 5. Return? Need to ask prof
+ */
+public class Weeder extends DepthFirstAdapter {
+}
