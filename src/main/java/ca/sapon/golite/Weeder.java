b5e18390f15158c18c9d7cb27afbb27f6d7d1dfd
Aleksi Sapon <aleksi@sapon.ca>
2017-02-23 18:30:12 -0500

    Stub out the rest of the weeder

--- src/main/java/ca/sapon/golite/Weeder.java
+++ src/main/java/ca/sapon/golite/Weeder.java
@@ -41,6 +41,59 @@ import golite.node.Node;
  * 5. Break and continue
  */
 public class Weeder extends DepthFirstAdapter {
+    private final Deque<Scope> scopeStack = new ArrayDeque<>();
+
+    @Override
+    public void inAProg(AProg node) {
+        scopeStack.push(Scope.TOP);
+    }
+
+    @Override
+    public void outAProg(AProg node) {
+        popScope(Scope.TOP);
+    }
+
+    @Override
+    public void inAFuncDecl(AFuncDecl node) {
+        scopeStack.push(Scope.FUNC);
+    }
+
+    @Override
+    public void outAFuncDecl(AFuncDecl node) {
+        popScope(Scope.FUNC);
+    }
+
+    @Override
+    public void inAIfStmt(AIfStmt node) {
+        scopeStack.push(Scope.IF);
+    }
+
+    @Override
+    public void outAIfStmt(AIfStmt node) {
+        popScope(Scope.IF);
+    }
+
+    @Override
+    public void inAForStmt(AForStmt node) {
+        scopeStack.push(Scope.FOR);
+    }
+
+    @Override
+    public void outAForStmt(AForStmt node) {
+        popScope(Scope.FOR);
+    }
+
+    @Override
+    public void inASwitchStmt(ASwitchStmt node) {
+        scopeStack.push(Scope.SWITCH);
+        // Check that case.getClass() == ADefaultCase.class is only true for one case at most
+    }
+
+    @Override
+    public void outASwitchStmt(ASwitchStmt node) {
+        popScope(Scope.SWITCH);
+    }
+
     @Override
     public void outAAssignStmt(AAssignStmt node) {
         node.getLeft().forEach(n -> n.apply(AssignableWeeder.INSTANCE));
@@ -100,6 +153,43 @@ public class Weeder extends DepthFirstAdapter {
     public void outAAssignBitXorStmt(AAssignBitXorStmt node) {
         node.getLeft().apply(AssignableWeeder.INSTANCE);
     }
+
+    @Override
+    public void outADeclVarShortStmt(ADeclVarShortStmt node) {
+        // Check that the left list elements are all getClass() == AIdentExpr.class
+    }
+
+    @Override
+    public void outAExprStmt(AExprStmt node) {
+        // Check that the expr node getClass() == ACallExpr.class
+    }
+
+    @Override
+    public void outABreakStmt(ABreakStmt node) {
+        // Check that scope stack contains FOR
+    }
+
+    @Override
+    public void outAContinueStmt(AContinueStmt node) {
+        // Check that scope stack contains FOR
+    }
+
+    @Override
+    public void outAReturnStmt(AReturnStmt node) {
+        // Check that scope stack contains FUNC
+    }
+
+    private void popScope(Scope out) {
+        final Scope scope = scopeStack.pop();
+        if (scope != out) {
+            throw new IllegalStateException("Expected out of scope " + out + ", but got " + scope);
+        }
+    }
+
+    private enum Scope {
+        TOP, FUNC, IF, FOR, SWITCH
+    }
+
     private static class AssignableWeeder extends AnalysisAdapter {
         private static final AssignableWeeder INSTANCE = new AssignableWeeder();
 
