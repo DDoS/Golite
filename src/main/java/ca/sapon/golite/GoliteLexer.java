c3aa801de864ec3a40d6a4e6ad62768a690472ee
Aleksi Sapon <aleksi@sapon.ca>
2017-02-24 17:32:58 -0500

    Fix single line comments breaking automatic semicolons

--- src/main/java/ca/sapon/golite/GoliteLexer.java
+++ src/main/java/ca/sapon/golite/GoliteLexer.java
@@ -8,6 +8,8 @@ import java.util.stream.Stream;
 
 import golite.lexer.Lexer;
 import golite.lexer.LexerException;
+import golite.node.TCommentMultiln;
+import golite.node.TCommentSingleln;
 import golite.node.TDblMinus;
 import golite.node.TDblPlus;
 import golite.node.TEndln;
@@ -27,12 +29,12 @@ import golite.node.TRhtParen;
 import golite.node.TRhtSqParen;
 import golite.node.TRuneLit;
 import golite.node.TSemi;
+import golite.node.TSpace;
 import golite.node.Token;
 
 /**
  * A modified lexer that supports automatic semicolon insertion, according to the Go rules.
  * <p>Based off the example shown in class.</p>
- * TODO: fix comments at the end of the line not breaking this
  */
 public class GoliteLexer extends Lexer {
     private Token previousToken = null;
@@ -43,11 +45,23 @@ public class GoliteLexer extends Lexer {
 
     @Override
     protected void filter() throws LexerException, IOException {
-        if (previousToken != null && token.getClass() == TEndln.class && FINAL_LINE_TOKENS.contains(previousToken.getClass())) {
+        final Class<? extends Token> tokenClass = token.getClass();
+        // Ignore the ignored tokens, except single lines comments because they end in a new line
+        if (tokenClass == TCommentMultiln.class || tokenClass == TSpace.class) {
+            return;
+        }
+        // If the token ends a line and the previous token matches certain rules, replace it by a semicolon
+        if (previousToken != null && END_LINE_TOKENS.contains(tokenClass)
+                && FINAL_LINE_TOKENS.contains(previousToken.getClass())) {
             token = new TSemi(token.getLine(), token.getPos());
         }
+        // Either way, set the current token as the previous one
         previousToken = token;
     }
+
+    private static final Set<Class<? extends Token>> END_LINE_TOKENS = Stream.of(
+            TEndln.class, TCommentSingleln.class
+    ).collect(Collectors.toSet());
 
     private static final Set<Class<? extends Token>> FINAL_LINE_TOKENS = Stream.of(
             TIdenf.class, TIntLit.class, THexLit.class, TOctLit.class, TFloatLit.class,
