c6342382ea49be6f57303e66fc5f03d6902fb5cb
Rohit <rohit.verma@mail.mcgill.ca>
2017-02-26 16:21:12 -0500

    original a.go

--- program/valid/a.go
+++ program/valid/a.go
@@ -1,15 +1,101 @@
-//Given int n, returns the factorial of n
-//n! = n*(n-1)*...*1
-package main
+package test
 
-func fact(n int) int {
-	fact := n
-	for n := n-1; n>1; n-- {
-		fact = fact * n
-	}
-	return fact
+/*
+    This file is just full of random garbage.
+*/
+
+var (
+    g = 1
+    b bool
+)
+
+var mul, m3 = + +43, g * 53
+var gf, fd int
+var lol int = (fd + 42) * (.42 * 4.2) * 42. * gh("lol") && h().u6 * (-+5.4) || append('l' &^ float64(g.h), rune << 4)
+
+type (
+    Point struct {
+        x, y, z float64
+        _ int
+        inr rune
+        _ int
+    }
+    float float64
+    chars ([]rune)
+)
+
+func hey(a, b int) {
+    a, _ := 0, 1
+    b = 2
+    _ = 3
+    f, _ = 4, 5
+    var (
+        _ = 4
+        gh, fd = - -45, "l"
+    )
+    type stuff [0xaF]Point
+    type _ []ignored
+
+    if b := 1; true {
+        a := 1
+    }
+
+    if c++; 5 + 6 {
+        return
+    } else {
+        print("stuff")
+    }
+
+    if c++; 5 + 6 {
+        return
+    } else if more {
+        print("stuff")
+    } else {
+        for {
+            (always) = true
+        }
+    }
+
+    if c++; 5 + 6 {
+        return
+    } else if more {
+        print("stuff")
+    } else if no - end + in / sight {
+        for i < 0 {
+            (somtimes())
+        }
+    }
+
+    if c++; 5 + 6 {
+        return
+    } else if more {
+        print("stuff")
+    } else if no - end + in / sight {
+        for i := 0; i < 10; i++ {
+            ten.times[1] = 2
+        }
+    } else {
+        actually(we, are, done)
+    }
+    ten.times[1]--
+    (x + 5)(x + 2)
+    a := (x + 5).test
+    a := (x + 5)[g.l]
+    a := !(x + 2)
+    a := ^(x + 2)
+    a := +(x + 2)
+    a := -(x + 2)
+    a := append(x, 4)[2]
 }
 
-func main() {
-	print(fact(5))
+func omn(a (bool), b int) ([4][]stuff) {
+    var _, _, a int
+}
+
+func hh(_ string, _ bool) struct {x int;} {
+
+}
+
+func _() {
+    // Not bound!
 }
