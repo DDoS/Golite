74b98bc0464845ce6903605d0176a292d44a2c07
Rohit <rohit_verma_10@yahoo.com>
2017-02-26 20:17:07 -0500

    A raw string test case

---
+++ program/valid_extra/raw_string.go
@@ -0,0 +1,11 @@
+//Raw String test
+
+package main
+
+func main() {
+
+  var s string
+s = `:~#$%^&*-+/\\<>=_|';:{}[]()\"\a\b  \f\n\r\t\v\\ ABCDEFGHIJKLMNOPQRSTUVW
+XYZ abcdefghijklmnopqrstuvwxyz 0123456789`
+
+}
