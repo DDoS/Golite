933f9115d4f0936620d6d66ea835de20098941fc
Aleksi Sapon <aleksi@sapon.ca>
2017-03-03 17:23:01 -0500

    Add "constant" flag to variable symbols

--- src/main/java/ca/sapon/golite/semantic/context/UniverseContext.java
+++ src/main/java/ca/sapon/golite/semantic/context/UniverseContext.java
@@ -9,8 +9,8 @@ import ca.sapon.golite.semantic.type.BasicType;
  */
 public final class UniverseContext extends Context {
     public static final UniverseContext INSTANCE = new UniverseContext();
-    private static final Variable TRUE_VARIABLE = new Variable("true", BasicType.BOOL);
-    private static final Variable FALSE_VARIABLE = new Variable("false", BasicType.BOOL);
+    private static final Variable TRUE_VARIABLE = new Variable("true", BasicType.BOOL, true);
+    private static final Variable FALSE_VARIABLE = new Variable("false", BasicType.BOOL, true);
 
     private UniverseContext() {
         super(null);
--- src/main/java/ca/sapon/golite/semantic/symbol/Variable.java
+++ src/main/java/ca/sapon/golite/semantic/symbol/Variable.java
@@ -7,18 +7,25 @@ import ca.sapon.golite.semantic.type.Type;
  */
 public class Variable extends Symbol {
     private final Type type;
+    private boolean constant;
 
-    public Variable(String name, Type type) {
+    public Variable(String name, Type type, boolean constant) {
         super(name);
         this.type = type;
+        this.constant = constant;
     }
 
-    public Variable(int startLine, int endLine, int startPos, int endPos, String name, Type type) {
+    public Variable(int startLine, int endLine, int startPos, int endPos, String name, Type type, boolean constant) {
         super(startLine, endLine, startPos, endPos, name);
         this.type = type;
+        this.constant = constant;
     }
 
     public Type getType() {
         return type;
+    }
+
+    public boolean isConstant() {
+        return constant;
     }
 }
