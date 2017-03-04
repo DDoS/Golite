bcde833cedd779f45faaafaa92ee62ebd7c13f76
Aleksi Sapon <aleksi@sapon.ca>
2017-03-04 16:55:27 -0500

    Add some more test code to bunch.go

--- programs/valid_extra/bunch.go
+++ programs/valid_extra/bunch.go
@@ -4,9 +4,16 @@ func test(b bool, i int) float64 {
 
 }
 
-type data []int
+type Data []int
+type Person struct {
+    name string
+    age int
+}
 
-var a data
+var a Data
+var b Person
 var c = test(true, 1)
 var d = int('a')
 var e = append(a, d)
+var f = b.age
+var g = e[0]
