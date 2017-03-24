0aeec5e9ea881e46834ff05b00ee010ed13cd850
Rohit <rohit_verma_10@yahoo.com>
2017-03-23 21:03:34 -0400

    Adding programs for Milestone 3, Q2

---
+++ programs/invalid/types/switch_codegen.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+    
+    var boolvar bool = true
+    var intvar int = 2
+    
+    switch boolvar {
+        case intvar > 1 : { 
+        print("Case 1")
+        } 
+    }
+}
---
+++ programs/invalid/types/switch_codegen_twobools.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+    switch {
+    case false, true:
+        x := 1
+        print(" In case true false with value ", x)
+        
+    default:
+        x := 2
+         print("In case default ", x)
+    }
+}
