82ab988678452366ddfcd7a3ec27ad8da88e5728
Rohit <rohit_verma_10@yahoo.com>
2017-03-23 22:55:32 -0400

    Adding programs for Q2, Milestone 3

---
+++ programs/code/array_default.go
@@ -0,0 +1,9 @@
+// Check for default value of float in an array
+
+package main
+
+var multidim [6][6]float64
+
+func main() {
+    print(multidim[2][2]) // Should print default initialization value 
+}
---
+++ programs/code/scope_var.go
@@ -0,0 +1,11 @@
+// Checking Variable Scopes
+
+package main
+
+var f float64 = 1.0
+
+func main() {
+	f := 2
+
+	print("Value of f is", f)
+}
